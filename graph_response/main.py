8eb842bf8552f508ae01a5c4d8da2b9bb0542f8c
isaiahb <isaiahballah@gmail.com>
2020-07-17 15:46:42 -0400

    Sort the queue impacts so the impacts where the new SLA is greater then the desired impact or increased from the previous SLA

--- graph_response/main.py
+++ graph_response/main.py
@@ -2,25 +2,41 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import impact_analysis_response_pb2
 
+# Sort the queue impacts so the impacts where the new SLA is greater then the desired impact or increased from the previous SLA
+def CompareImpact(queue_impact):
+    desired_delta = (queue_impact.desired_SLA_min - queue_impact.new_SLA_min) + (queue_impact.previous_SLA_min - queue_impact.new_SLA_min)
+    return desired_delta
+
+
 def GraphImpact(impact_analysis):
-	data = {"desired sla": {}, "old sla" : {}, "new sla" : {}}
+	data = {"Desired SLA": {}, "Previous SLA" : {}, "New SLA" : {}}
 	i = 0
-	for queue_impact in impact_analysis.queue_Impact_analysis_list:
+	queue_impact_list = impact_analysis.queue_Impact_analysis_list
+	queue_impact_list.sort(key=CompareImpact)
+	for queue_impact in queue_impact_list:
 		i = i + 1
 		id = queue_impact.queue_id or i
-		data["desired sla"][id] = queue_impact.desired_SLA_min
-		data["old sla"][id] = queue_impact.previous_SLA_min
-		data["new sla"][id] = queue_impact.new_SLA_min
+		data["Desired SLA"][id] = queue_impact.desired_SLA_min
+		data["Previous SLA"][id] = queue_impact.previous_SLA_min
+		data["New SLA"][id] = queue_impact.new_SLA_min
 
 	df = pd.DataFrame(data)
+	ax = df.plot(kind='bar')
 
-	df.plot(kind='bar')
+	# Set the y axis label
+	ax.set_ylabel('SLA (minutes)')
+
+	# Set the x axis label
+	ax.set_xlabel('Queue Id')
+
+	# Set the chart's title
+	ax.set_title('Queue Impact Analysis')
 
 	plt.show()
 
 
 impact_analysis = impact_analysis_response_pb2.ImpactAnalysisResponse()
-f = open("/usr/local/google/home/ballah/Documents/dev/youtube-hermes-config/graph_response/impact.txt", "rb")
+f = open("/Users/isaiah/Dev/Google/youtube-hermes-config/graph_response/impact.txt", "rb")
 impact_analysis.ParseFromString(f.read())
 f.close()
 
