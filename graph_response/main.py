94c2f9a18fdf6bb6f2ee9e98058c1ceff942a259
ballah <isaiahballah@gmail.com>
2020-08-07 14:41:54 -0400

    addressed pull request comments, punctuated comments, removed unnecessary comments and file

--- graph_response/main.py
+++ graph_response/main.py
@@ -2,18 +2,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import impact_analysis_response_pb2
 
-# Sort the queue impacts so the impacts where the new SLA is greater then the desired impact or increased from the previous SLA
+# Sort the queue impacts so the impacts where the new SLA is greater then the desired impact or increased from the previous SLA.
 def CompareImpact(queue_impact):
     desired_delta = (queue_impact.desired_SLA_min - queue_impact.new_SLA_min) + (queue_impact.previous_SLA_min - queue_impact.new_SLA_min)
     return desired_delta
 
 
+# This function takes a ImpactAnalysisResponse object and displays the Queues Impact Graph.
 def GraphImpact(impact_analysis):
 	data = {"Desired SLA": {}, "Previous SLA" : {}, "New SLA" : {}}
-	i = 0
 	queue_impact_list = impact_analysis.queue_Impact_analysis_list
 	queue_impact_list.sort(key=CompareImpact)
+
+	i = 0
 	for queue_impact in queue_impact_list:
+		# i variable used to insure the queue_id property is valid, and will be cleaned up later.
 		i = i + 1
 		id = queue_impact.queue_id or i
 		data["Desired SLA"][id] = queue_impact.desired_SLA_min
@@ -23,22 +26,8 @@ def GraphImpact(impact_analysis):
 	df = pd.DataFrame(data)
 	ax = df.plot(kind='bar')
 
-	# Set the y axis label
 	ax.set_ylabel('SLA (minutes)')
-
-	# Set the x axis label
 	ax.set_xlabel('Queue Id')
-
-	# Set the chart's title
 	ax.set_title('Queue Impact Analysis')
 
 	plt.show()
-
-
-impact_analysis = impact_analysis_response_pb2.ImpactAnalysisResponse()
-f = open("/Users/isaiah/Dev/Google/youtube-hermes-config/graph_response/impact.txt", "rb")
-impact_analysis.ParseFromString(f.read())
-f.close()
-
-print(impact_analysis)
-GraphImpact(impact_analysis)
